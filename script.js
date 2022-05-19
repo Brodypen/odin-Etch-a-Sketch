c367002178fba5ba41fbe75cf56a9b521658aad6
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-19 16:05:20 -0500

    FIx gridLine bug

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 let color = "#000000";
+let eraserToggler = false;
 makeGrid(16);
-function makeGrid(size){
-    etchGrid = document.getElementById("etchGrid");
-    while(etchGrid.firstChild){
-        etchGrid.removeChild(etchGrid.lastChild);
-    }
-    etchGrid.style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
-    etchGrid.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
-    block = document.createElement("div");
-    block.classList.add('gridBlock', 'gridBlockBorder');
-    for(let i = 0; i < size * size; i++){
-    etchGrid.appendChild(block.cloneNode(true));
-    }
-}
 
+const sizeSlider = document.querySelector(".sizePicker");
+let size = document.querySelector(".sizePicker").value;
+sizeSlider.onmousemove = (e) => changeSizeText()
+function makeGrid(size) {
+  etchGrid = document.getElementById("etchGrid");
+  while (etchGrid.firstChild) {
+    etchGrid.removeChild(etchGrid.lastChild);
+  }
+  etchGrid.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
+  etchGrid.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
+  block = document.createElement("div");
+  block.classList.add("gridBlock", "gridBlockBorder");
+  block.addEventListener('mousedown', changeBlock);
+  for (let i = 0; i < size * size; i++) {
+    etchGrid.appendChild(block.cloneNode(true));
+  }
+}
+function changeBlock(e){
+
+}
 function rainbowToggle() {}
 function darkenToggle() {}
 function gridLinesToggle() {
-    const collection = document.getElementsByClassName('gridBlock');
-    const size = document.querySelector(".sizePicker").value;
-    console.log(collection);
-    for(let i = 0; i < size * size; i++){
-        collection[i].classList.toggle('gridBlockBorder');
-    }
+  const collection = document.getElementsByClassName("gridBlock");
+  console.log(collection);
+  for (let i = 0; i < size * size; i++) {
+    collection[i].classList.toggle("gridBlockBorder");
+  }
 }
-function eraserToggle(){
+function eraserToggle() {
 
 }
 function changeSize() {
-  const size = document.querySelector(".sizePicker").value;
+  size = document.querySelector(".sizePicker").value;
   makeGrid(size);
 }
 function changeSizeText() {
-  const size = document.querySelector(".sizePicker").value;
+  const sizeDisplay = document.querySelector(".sizePicker").value;
   sizeText = document.querySelector(".sizePickerText");
-  sizeText.innerHTML = "Size: " + size + "x" + size;
+  sizeText.innerHTML = "Size: " + sizeDisplay + "x" + sizeDisplay;
 }
 function clearGrid() {}
