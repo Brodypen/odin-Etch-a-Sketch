e9466d22dca22a6c8e2083a165eef8b2c22d6c7b
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-19 13:35:59 -0500

    Add changeSize and changeSizeText

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-
-function rainbowToggle(){
-
-}
-function darkenToggle(){
-
-}
-function gridLinesToggle(){
+let color = "#000000";
+function rainbowToggle() {}
+function darkenToggle() {}
+function gridLinesToggle() {}
+function eraserToggle(){
 
 }
-function clearGrid(){
-
+function changeSize() {
+  const size = document.querySelector(".sizePicker").value;
+  console.log(size);
 }
+function changeSizeText() {
+  const size = document.querySelector(".sizePicker").value;
+  sizeText = document.querySelector(".sizePickerText");
+  sizeText.innerHTML = "Size: " + size + "x" + size;
+}
+function clearGrid() {}
